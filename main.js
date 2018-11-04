4e7d5ae6e698d45f6be05bfaf0ab1a31c99dca92
rizeon <danieljoris08@gmail.com>
2018-11-04 18:28:48 -0200

    Add local storage save

--- main.js
+++ main.js
@@ -2,20 +2,51 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Adotar um gatinho',
-    'Comer um montão',
-    'Dar uma voltinha de bike'
-];
+var todos = JSON.parse(localStorage.getItem('todosList')) || [];
 
-function renderTodos(){
+function renderTodos() {
+    listElement.innerHTML = '';
+
     for (todo of todos) {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
 
+        var linkElement = document.createElement('a');
+        
+        linkElement.setAttribute('href', '#');
+
+        var position = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo('+ position + ')');
+
+        var linkText = document.createTextNode('Del');
+        linkElement.appendChild(linkText);
+
         todoElement.appendChild(todoText);
         listElement.appendChild(todoElement);
+        todoElement.appendChild(linkElement);
     }
 }
 
 renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+
+    renderTodos();
+    saveToStorage();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('todosList', JSON.stringify(todos));
+}
