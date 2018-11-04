b61a755f79ff141594736291fa3fdf9a4f14fd38
rizeon <danieljoris08@gmail.com>
2018-11-04 16:36:36 -0200

    Core files

---
+++ main.js
@@ -0,0 +1,9 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Adotar um gatinho',
+    'Comer um montão',
+    'Dar uma voltinha de bike'
+];
